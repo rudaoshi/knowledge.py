d7ce34d5ab6a85d793df1feee8e0d70e5e60cd2c
孙明明 <sunmingming@gmail.com>
2015-04-15 22:35:12 +0800

    Merge branch 'master' of https://github.com/rudaoshi/knowledge.py
    # Conflicts:
    #	knowledge/interface/train_dnn_for_big_data.py
    #	knowledge/machine/cost/cross_entropy.py
    #	knowledge/machine/neuralnetwork/mlp.py
    #	knowledge/machine/neuralnetwork/neuralnet_factory.py
    #	knowledge/machine/optimization/cgd_optimizer.py
    #	knowledge/machine/optimization/optim_factory.py
    #	knowledge/machine/optimization/sgd_optimizer.py
    #	knowledge/util/theano_util.py

--- knowledge/util/hadoop.py
+++ knowledge/util/hadoop.py
@@ -4,11 +4,14 @@ __author__ = 'Sun'
 import os
 def download_file(hadoop_bin, file_path, save_dir):
 
+    downloaded_file_path = os.path.join(save_dir, os.path.basename(file_path))
+
+    os.system("rm " + downloaded_file_path)
+
     return_code = os.system(" ".join([hadoop_bin, " fs -get ", file_path, save_dir]))
 
     if return_code != 0:
         raise Exception("Cannot download file " + file_path)
 
-    downloaded_file_path = os.path.join(save_dir, os.path.basename(file_path))
 
     return downloaded_file_path
